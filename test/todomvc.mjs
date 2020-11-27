492b29957f7a34a3108606fd714b918900d758fd
Niklas Fasching <niklas.fasching@gmail.com>
2020-11-27 23:06:24 +0100

    Start of todomvc implementation

--- test/todomvc.mjs
+++ test/todomvc.mjs
@@ -7,10 +7,32 @@ t.describe("TodoMVC", () => {
   });
 
   t.describe("New Todo", () => {
-    t("should add todo items");
-    t("should clear input after an item is added");
-    t("should trim entered text");
-    t("should show #main and #footer when there are todos");
+    let page, input, items;
+    t.before(async () => {
+      page = await getPage("./todomvc.html");
+      input = await page.first("input", 10);
+      input.value = "  take over the world  ";
+      enter(input);
+      items = await page.all(".todo-item");
+    });
+
+    t("should add todo items", () => {
+      t.assertEqual(items.length, 1);
+    });
+
+    t("should clear input after an item is added", () => {
+      t.assertEqual(items.length, 1);
+      t.assertEqual(input.value, "");
+    });
+
+    t("should trim entered text", () => {
+      t.assertEqual(items[0].innerHTML, "take over the world");
+    });
+
+    t("should show #main and #footer when there are todos", async () => {
+      t.assert(await page.first("#main"));
+      t.assert(await page.first("#footer"));
+    });
   });
 
   t.describe("Mark all as completed", () => {
@@ -56,3 +78,40 @@ t.describe("TodoMVC", () => {
     t("should highlight the currently active filter");
   });
 });
+
+
+function getPage(src) {
+  return new Promise((resolve, reject) => {
+    const iframe = document.createElement("iframe");
+    const onerror = reject;
+    const onload = () => resolve(new Page(iframe));
+    document.body.appendChild(Object.assign(iframe, {onload, onerror, src}));
+  });
+}
+
+
+class Page {
+  constructor(iframe) {
+    this.iframe = iframe;
+    this.window = iframe.contentWindow;
+    this.document = iframe.contentDocument;
+  }
+
+  first(selector, timeout)  { return this.waitFor(selector, "", timeout) }
+
+  all(selector, timeout) { return this.waitFor(selector, "All", timeout) }
+
+  async waitFor(selector, maybeAll, timeout = 0) {
+    const now = Date.now();
+    while (true) {
+      const x = this.document["querySelector" + maybeAll](selector);
+      if (maybeAll ? x.length : x) return x;
+      else if (Date.now() - now > timeout) throw new Error(`timeout waiting for "${selector}"`);
+      else await new Promise(r => requestAnimationFrame(r));
+    }
+  }
+}
+
+function enter(node, type = "keydown") {
+  node.dispatchEvent(new KeyboardEvent(type, {key: "Enter"}));
+}
