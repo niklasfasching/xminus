68e58a7bf194edfc7a55615ec79f32cb8365c312
Niklas Fasching <niklas.fasching@gmail.com>
2020-11-29 19:41:42 +0100

    test: Add support for only / selecting tests

--- src/test.mjs
+++ src/test.mjs
@@ -1,19 +1,25 @@
-let node = newNode(), count = 0, countFailed = 0, resolve;
+const root = newNode();
+let node = root, count = 0, countFailed = 0, resolve;
 
 export const done = new Promise((r) => resolve = r);
 
 export function t(name, f) {
-  node.children.push({name, f});
+  node.children.push({name, f, selected: node.selected});
 }
 
 Object.assign(t, {
   describe(name, f) {
-    const parent = node;
-    node = newNode(name);
-    const result = f?.();
-    if (result) throw new Error(`unexpected return value from describe: ${result}`);
-    parent.children.push(node);
-    node = parent;
+    group(name, f);
+  },
+
+  describeOnly(name, f) {
+    group(name, f, true);
+    markNodes(node, "hasSelected");
+  },
+
+  only(name, f) {
+    node.children.push({name, f, selected: true});
+    markNodes(node, "hasSelected");
   },
 
   before(name, f) {
@@ -43,20 +49,29 @@ Object.assign(t, {
   },
 });
 
-async function run(lvl, node) {
-  const start = performance.now();
-  if (lvl) log(lvl, "", node.name);
-  for (let {name, f} of node.befores) await runFn(lvl+2, name, f, false);
-  for (let child of node.children) {
-    if (child.children) await run(lvl+2, child);
-    else await runFn(lvl+2, child.name, child.f, true);
-  }
-  for (let {name, f} of node.afters) await runFn(lvl+2, name, f, false);
-  if (lvl) log(lvl, "color: grey", `(${(performance.now() - start).toFixed()}ms)\n`);
-  else log(lvl + 2, "color: grey", `${count} tests (${countFailed} failures)\n`);
+function group(name, f, selected) {
+  node = newNode(name, node, selected);
+  const result = f?.();
+  if (result) throw new Error(`unexpected return value from describe: ${result}`);
+  node.parent.children.push(node);
+  node = node.parent;
 }
 
-async function runFn(lvl, name, f, isTest) {
+async function run(lvl, node) {
+  const start = performance.now(), selected = !root.hasSelected || node.selected || node.hasSelected;
+  if (selected && node !== root) log(lvl, "", node.name);
+  for (let {name, f} of node.befores) await runFn(lvl+2, name, f, false, selected);
+  for (let child of node.children) {
+    if (child.children) await run(lvl+2, child);
+    else await runFn(lvl+2, child.name, child.f, true, !root.hasSelected || child.selected);
+  }
+  for (let {name, f} of node.afters) await runFn(lvl+2, name, f, false, selected);
+  if (selected && node !== root) log(lvl, "color: grey", `(${(performance.now() - start).toFixed()}ms)\n`);
+  else if (node === root) log(lvl + 2, "color: grey", `${count} tests (${countFailed} failures)\n`);
+}
+
+async function runFn(lvl, name, f, isTest, selected) {
+  if (!selected) return;
   if (isTest) count++;
   const start = performance.now();
   try {
@@ -74,8 +89,16 @@ function log(lvl, color, line, ...lines) {
   for (let l of lines) console.info(" ".repeat(lvl + 2) + "%c" + l, "color: grey");
 }
 
-function newNode(name) {
-  return {name, children: [], befores: [], afters: []};
+function newNode(name, parent, selected) {
+  return {name, parent, selected, children: [], befores: [], afters: []};
+}
+
+function markNodes(node, key) {
+  do { node[key] = true } while (node = node.parent);
+}
+
+function isSelected(node) {
+  return root.hasSelected ? node.selected || node.hasSelected : true;
 }
 
 setTimeout(async () => {
