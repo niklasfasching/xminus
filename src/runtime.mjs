409ac16f90edf7a722d238cadb3d24ff1a5e9549
Niklas Fasching <niklas.fasching@gmail.com>
2021-05-31 12:36:04 +0200

    runtime: Fix xSlot extraction
    appending the child to the slot removes it from this and thus changes the
    length of childNodes. for of loop doesn't seem to like that and leaves children
    behind

--- src/runtime.mjs
+++ src/runtime.mjs
@@ -121,7 +121,7 @@ export function register(name, html, f, assignedProps) {
       this.app = app;
       this.props = props;
       this.xSlot = slotTemplate.cloneNode(true);
-      for (let child of this.childNodes) this.xSlot.append(child);
+      while (this.firstChild) this.xSlot.append(this.firstChild);
       for (let k of assignedProps) this[k] = this.props[k];
       this.onInit(this.props);
       this.append(document.importNode(template.content, true));
