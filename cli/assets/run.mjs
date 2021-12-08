cae72930e3bd853a504b7b3e8fe78342055b0520
Niklas Fasching <niklas.fasching@gmail.com>
2021-12-08 02:01:03 +0100

    Misc cleanup / refactor

--- cli/assets/run.mjs
+++ cli/assets/run.mjs
@@ -4,8 +4,7 @@ if (!isHeadless) window.close = (code = 0) => console.log("exit:", code);
 window.openIframe = (src) => {
   return new Promise((resolve, reject) => {
     const iframe = document.createElement("iframe");
-    const onerror = reject;
-    const onload = () => resolve(iframe);
+    const onload = () => resolve(iframe), onerror = reject;
     document.body.appendChild(Object.assign(iframe, {onload, onerror, src}));
   });
 };
