8a2e8e3d9fe6124ffb4b07a57f3595a454e45ed8
Niklas Fasching <niklas.fasching@gmail.com>
2022-08-31 19:53:33 +0200

    examples: Remove hoerdle
    Turns out spotify forbids using their api to create music guessing games (thx i
    guess) and the selection they have right now is shitty enough that i can't be
    bothered to continue to make it work just for that and not custom playlists.

--- examples/hoerdle/fetch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-curl https://www-growth.scdn.co/_next/static/chunks/pages/heardle-735a6a1069ced864.js |\
-    grep -Po '{"artist":.*?transcoded.*?}' |\
-    sed 's/\\'/'/g' |\
-    jq -s > tracks.json
